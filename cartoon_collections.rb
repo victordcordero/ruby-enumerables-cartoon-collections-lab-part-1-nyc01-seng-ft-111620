cf89ec19d49746fd78e2f59d388a43eb80b9f0e2
Victor Cordero <victordcordero@gmail.com>
2020-11-10 03:05:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |value, index|
-    next if index == 0
+  
     puts "#{index}.  #{value}"
 end
 end
